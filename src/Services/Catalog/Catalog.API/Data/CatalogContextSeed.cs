a560cf1c5166972a3e3fd1cf67a8eab59d4d7c10
Maximiliano Menna <mmenna907@gmail.com>
2021-10-15 09:53:47 -0300

    Desarrollo capa de negocio empleando Patron Repositorio para Catalog.API

--- src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -13,11 +13,8 @@ namespace Catalog.API.Data
         {
             bool existProduct = productCollection.Find(p => true).Any();
             if (!existProduct)
-            {
                 productCollection.InsertManyAsync(GetPreconfiguredProducts());
-            }
         }
-
         private static IEnumerable<Product> GetPreconfiguredProducts()
         {
             return new List<Product>()
